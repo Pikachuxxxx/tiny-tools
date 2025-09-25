998a2c8a534f9c9a0a5be6389d10dd84c594e3cb
Pikachuxxxx <phani.s2909@icloud.com>
2025-09-26 01:58:43 +0530

    add gitignore

--- build/CMakeFiles/run_tiny_c.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_tiny_c"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_tiny_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
